214c20088d41c17f255e02f891fd17b4a33277e4
Sony Darmawan <sonypiay@mail.com>
2020-04-11 12:39:55 +0700

    initial commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,3 @@
+<template>
+	<div id="navbar"></div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<div class="uk-height-large uk-background-cover uk-flex" uk-parallax="bgy: -200" :style="{
+			'background-image': `url('../assets/images/dark.jpg')`
+		}">
+			<h1 class="uk-width-1-2@m uk-text-center uk-margin-auto uk-margin-auto-vertical">Headline</h1>
+		</div>
+	</div>
+</template>
+
+<script type="text/javascript">
+	export default {
+		name: 'home',
+		computed: {
+			url() {
+				return document.location.origin;
+			}
+		}
+	}
+</script>
